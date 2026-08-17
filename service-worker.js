b0f1b98366278fea7834868657075a30519e70d4
Thomas <thomi_mariani@hotmail.com>
2026-08-16 23:43:11 -0300

    Actualizar cache de la aplicacion

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lista-familiar-v1";
+const CACHE_NAME = "lista-familiar-v2";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon-512.png"];
 self.addEventListener("install", (event) => { event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))); self.skipWaiting(); });
 self.addEventListener("activate", (event) => { event.waitUntil(self.clients.claim()); });
